260fb8a2de9c9c1f9c9cc62d95e841877cdefbc8
Tiffany <tchan12899@gmail.com>
2025-05-17 22:11:01 -0400

    Structured home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,4 +1,20 @@
 // This file contains code that will render the Home page/landing page
-import React from `react`;
+import React, { useEffect, useState } from 'react';
 import './Home.css';
 
+// useEffect for doing something after component renders, e.g., fetching data from API, updating the DOM, storing something in localstorage
+
+const Home = () => {
+
+    // const navigate = useNavigate();
+
+    return (
+        <div>
+            <h1>Sign-in</h1>
+            <h1>Discussion</h1>
+        </div>
+    )
+
+};
+
+export default Home;
