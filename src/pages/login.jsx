358cadcb218cbf29feb832ac605d8b45a04baad3
Jasmine Lee <jasminelee_0409@hotmail.com>
2025-05-20 18:56:41 -0700

    Added useState hook to track login form input

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,1 +1,12 @@
- import React from 'react';
+import React, { useEffect, useState } from 'react';
+ 
+const LoginPage = () => {
+    //Creating variables for the user input fields and initializing them with empty strings.
+    const [loginData, setLoginData] = useState({
+        username: '',
+        password: '',
+    });
+
+};
+
+export default LoginPage;
